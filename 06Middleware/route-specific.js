99d14932ebe70f2448fe84dc16953d8e676ad3e0
sourabhchalke <sourabhchalke2157@gmail.com>
2024-12-08 18:15:21 +0530

    route-specific file updated

--- 06Middleware/route-specific.js
+++ 06Middleware/route-specific.js
@@ -4,8 +4,10 @@ const app=express();
 app.get('/user/:id',(req,res,next)=>{
     res.send("User Info....");
     next();
-}),(req,res)=>{
+});
+app.get('/home/',(req,res)=>{
     res.send("Second route...");
-}
+})
+
 
 app.listen(8012);
