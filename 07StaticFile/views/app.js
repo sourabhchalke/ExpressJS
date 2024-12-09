57fd1be3f8da937f85bd5afb8056e8a8fc9225f6
sourabhchalke <sourabhchalke2157@gmail.com>
2024-12-09 18:49:26 +0530

    express.static middleware added

--- 07StaticFile/views/app.js
+++ 07StaticFile/views/app.js
@@ -1,7 +1,16 @@
 const express=require('express');
 const app=express();
+const path=require('path');
 
+console.log(__dirname);
+console.log(path.join(__dirname,'../public/'));
 
+const staticPath=path.join(__dirname,'../public');
+
+// app.use(express.static());
+console.log(express.static(path.join(__dirname,'../public')));
+app.use(express.static(staticPath));
+// console.log(app.use(express.static(staticPath)));
 
 app.get('/',(req,res)=>{
     res.send("Hello");
