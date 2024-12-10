15fc20653252eb6595020204b8fbd49b9b3bd1bd
sourabhchalke <sourabhchalke2157@gmail.com>
2024-12-10 16:02:23 +0530

    EJS template engine used

--- 08TempEngine/ejs/app.js
+++ 08TempEngine/ejs/app.js
@@ -1,10 +1,13 @@
-const express=require('express');
-const app=express();
-const path=require('path');
+const express = require('express');
+const app = express();
 
+app.set('view engine', 'ejs'); // Set the view engine to ejs
+// app.set('views', 'views');  // Set the correct views directory
 
-app.get('/',(req,res)=>{
-    res.send("Hello...");
-})
+app.get('/', (req, res) => {
+    res.render('index',{title:"Template Engine"});
+});
 
-app.listen(8014);
+app.listen(8014, () => {
+    console.log('Server is running on port 8014');
+});
