02630b8efb878c71ed5daf1ca0aa99d36fdd0c60
sourabhchalke <sourabhchalke2157@gmail.com>
2024-12-13 14:19:14 +0530

    error handling with next middleware

---
+++ 11Asynchronous/next_error_handling.js
@@ -0,0 +1,27 @@
+const express=require('express');
+const app=express();
+
+function fetchData(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("Data Fetched");
+        },4000);
+    });
+}
+
+app.get('/',async(req,res,next)=>{
+    
+    try{
+        const data= await fetchData();
+        res.send(data);
+    }catch(err){
+        console.log("Something went wrong");
+        next(err);
+    }
+})
+app.use((err,req,res,next)=>{
+    console.log(err.stack);
+    res.status(500).send("Something went wrong");
+})
+
+app.listen(8017);
