e213c08365cb735d08f461fa9ba2f1f4f8109eb5
sourabhchalke <sourabhchalke2157@gmail.com>
2024-12-18 15:09:50 +0530

    connected to mongodb

---
+++ 14DB-Connection/api/mongodb.js
@@ -0,0 +1,18 @@
+const {MongoClient}=require('mongodb');
+const url="mongodb://localhost:27017";
+const database = "mongodb-demo";
+const client = new MongoClient(url);
+
+async function dbConnect(){
+    const db = await client.connect();
+    console.log("Connection Established");
+    const datab =  db.db(database);
+    console.log("Connected to Database");
+    const result = datab.collection('products');
+    console.log("Connected to Collection");
+    const response =await result.find().toArray();
+    console.log(response);
+
+}
+
+dbConnect();
