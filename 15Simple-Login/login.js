59a868880b3d3abc8ff363b2ddd8ad2d506bf1a6
sourabhchalke <sourabhchalke2157@gmail.com>
2024-12-20 12:54:18 +0530

    inserting form data into database

--- 15Simple-Login/login.js
+++ 15Simple-Login/login.js
@@ -1,11 +1,66 @@
-const express=require('express');
+const express = require('express');
 const router = express.Router();
+const dbConnect = require('./dbconnection/mongodb.js');
+const dbConnectLogin = require('./dbconnection/mongodblogin.js');
 
-router.get('/',(req,res)=>{
+router.get('/', (req, res) => {
     res.render('login');
 })
-router.post('/',(req,res)=>{
-    res.send("Login Successfull");
+router.post('/', async (req, res) => {
+
+    //Data getting from user input
+    let {username,password}=req.body;
+    console.log(username,password);
+
+    try {
+        
+        //Connecting to Resgistration collection to check user exists or not
+        let dbregister = await dbConnect();
+        const response = await dbregister.findOne({
+            username: username,
+        });
+        const response2 = await dbregister.findOne({
+            password: password,
+        });
+
+        //If exists then what to do 
+        if(response && response2){
+            console.log(response);
+            console.log(response2);
+            res.send("Login Successfull");
+
+            //Checking user first time login or before exists
+            let dblogin=await dbConnectLogin();
+            //getting data from login database
+            const response3 = await dblogin.findOne({
+                username: username,
+            });
+            const response4 = await dblogin.findOne({
+                password: password,
+            });
+            
+            //if exists then do not insert user details again in database
+            if(response3 && response4){
+                console.log("User Exists");
+            }else{
+                dblogin.insertOne({
+                    username:username,
+                    password:password
+                })
+                console.log("New Login")
+                console.log("Data Inserted");
+            }
+
+        }else{
+            res.send("Invalid Username or Password");
+            console.log(response);
+            
+        }
+    }catch(error){
+        console.error(error.stack);
+        res.status(400).send("Invalid Username or Password...");
+    }
+
 })
 
-module.exports=router;
+module.exports = router;
--- 15Simple-Login/register.js
+++ 15Simple-Login/register.js
@@ -1,15 +1,30 @@
 const express=require('express');
 const router = express.Router();
+const dbConnect=require('./dbconnection/mongodb.js');
 
 router.get('/',(req,res)=>{
     res.render('registration');
     
 })
 
-router.post('/',(req,res)=>{
-    res.send("Form submitted successfully");
+router.post('/',async(req,res)=>{
+    
+    res.send("Submitted Succesfully");
     const {first_name,last_name,username,email,password}=req.body;
-    console.log(first_name);
+    console.log(first_name,last_name,username,email,password);
+
+    let db= await dbConnect();
+    db.insertMany(
+        [{
+            first_name:first_name,
+            last_name:last_name,
+            username:username,
+            email:email,
+            password:password
+
+        }]
+    )
+    console.log("Data Inserted");
 })
 
 module.exports=router;
