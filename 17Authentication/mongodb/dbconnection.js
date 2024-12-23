eb566acc4f264d7f7bec4b4e8a61baba3462fd63
sourabhchalke <sourabhchalke2157@gmail.com>
2024-12-23 14:04:25 +0530

    database connected and schema and model created

---
+++ 17Authentication/mongodb/dbconnection.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const url="mongodb://localhost:27017/authentication";
+
+mongoose.connect(url)
+.then(()=>{console.log("Database Connected")})
+.catch((error)=>{console.log("Database not connected",error.stack)});
+
+const registrationSchema = new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required:true
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true
+        },
+        password:{
+            type:String,
+            unique:true
+        },
+        mob_no:{
+            type:Number
+        }
+    }
+);
+module.exports=registrationSchema;
+
+const loginSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        unique:true
+    }
+});
+module.exports=loginSchema;
