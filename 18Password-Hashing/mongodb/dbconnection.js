5bb0c02b674ff0e5ca9a69b8602b0e84b34d4720
sourabhchalke <sourabhchalke2157@gmail.com>
2024-12-26 11:21:27 +0530

    registration schema added

---
+++ 18Password-Hashing/mongodb/dbconnection.js
@@ -0,0 +1,22 @@
+const mongoose=require('mongoose');
+
+const userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+    },
+    username:{
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    age:{
+        type:Number,
+    },
+});
+
+const User= new mongoose.model('password_hashing',userSchema);
+module.exports=User;
