1099f769386688fc21fabc558fdb02510b6033a6
sourabhchalke <sourabhchalke2157@gmail.com>
2024-12-27 10:35:11 +0530

    handling multiple files with same filed name

--- 19Multer/app.js
+++ 19Multer/app.js
@@ -7,29 +7,50 @@ app.use(express.urlencoded({extended:false}));
 
 app.set('view engine','ejs');
 
-app.get('/',(req,res)=>{
-    res.render('fileupload');
-});
+// app.get('/',(req,res)=>{
+//     res.render('fileupload');
+// });
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './uploads');
-    },
-    filename: function (req, file, cb) {
-      cb(null,`${Date.now()}-${file.originalname}`);
-    }
-  })
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, './uploads');
+//     },
+//     filename: function (req, file, cb) {
+//       cb(null,`${Date.now()}-${file.originalname}`);
+//     }
+//   })
   
-const upload = multer({ storage });
+// const upload = multer({ storage });
 
 
-app.post('/upload',upload.single('fileupload'),(req,res)=>{
-  const file=req.body;
-  console.log(req.body);
-  console.log(req.file);
-  res.send("File Upload Succesfully");
+// app.post('/upload',upload.single('fileupload'),(req,res)=>{
+
+//   console.log(req.body);
+//   console.log(req.file);
+//   res.send("File Upload Succesfully");
+// })
+
+// Multiple files uploading
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(null,`${Date.now()}-${file.originalname}`);
+  }
 })
-
+const upload2 = multer({ storage });
+app.get('/multiple',(req,res)=>{
+  res.render('multiple');
+ 
+})
+app.post('/upload-multiple',upload2.array("fileupload1"),(req,res)=>{
+  
+  console.log(req.body);
+  console.log(req.files);
+  res.send("File Upload Succesfully");
+ 
+})
 
 
 app.listen(PORT,()=>{
